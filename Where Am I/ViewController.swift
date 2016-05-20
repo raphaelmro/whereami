22da00a8181dfc7830b2bb70404fcbc0af9dd025
Raphael <raphael.onofre@gmail.com>
2016-05-20 14:13:47 -0300

    Where Am I app commit

--- Where Am I/ViewController.swift
+++ Where Am I/ViewController.swift
@@ -6,14 +6,82 @@
 //  Copyright © 2016 Raphael Onofre. All rights reserved.
 //
 
+import MapKit
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    @IBOutlet weak var latitudeResult: UILabel!
+    @IBOutlet weak var longitudeResult: UILabel!
+    @IBOutlet weak var courseResult: UILabel!
+    @IBOutlet weak var speedResult: UILabel!
+    @IBOutlet weak var altitudeResult: UILabel!
+    @IBOutlet weak var nearestAddResult: UILabel!
+    @IBOutlet weak var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
+    
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations[0]
+        
+        let userLat = userLocation.coordinate.latitude
+        let userLon = userLocation.coordinate.longitude
+        
+    
+        let latitude: CLLocationDegrees = userLat
+        let longitude: CLLocationDegrees = userLon
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        let span = MKCoordinateSpanMake(latDelta, lonDelta)
+        let location = CLLocationCoordinate2DMake(latitude, longitude)
+        let region = MKCoordinateRegionMake(location, span)
+        
+        self.map.setRegion(region, animated: true)
+        
+        //setting data into view
+        latitudeResult.text = latitude.description
+        longitudeResult.text = longitude.description
+        courseResult.text = userLocation.course.description
+        speedResult.text = userLocation.speed.description
+        altitudeResult.text = userLocation.altitude.description
+        //setting nearest address
+        CLGeocoder().reverseGeocodeLocation(manager.location!) { (placemark, error) in
+            print(location)
+            
+            if placemark!.count > 0 {
+                let pm = placemark![0] as CLPlacemark
+                self.displayLocationInfo(pm)
+            } else{
+                print("Problem with geocoder")
+            }
+        }
+    }
+    
+    func displayLocationInfo(placemark: CLPlacemark){
+        locationManager.stopUpdatingLocation() //stop updating location to save battery life
+        print(placemark.locality)
+        print(placemark.postalCode )
+        print(placemark.administrativeArea)
+        print(placemark.country)
+        
+        let nearestAddress = placemark.locality! + " " + placemark.postalCode! + " - " + placemark.administrativeArea! + " " + placemark.country!
+        
+        nearestAddResult.text = nearestAddress
+        
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
